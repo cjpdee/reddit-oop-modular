ea4846a16e1f0b1514b4138dbbb446d2db95c4be
Charlie Dunderdale <cjpdee@gmail.com>
2018-11-22 12:43:18 +0000

    Finished webpack setup

--- dist/app.js
+++ dist/app.js
@@ -67,9 +67,91 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-(function webpackMissingModule() { throw new Error("Cannot find module \"/Users/Charlie/Code/reddit-oop-modular/src/app.js\""); }());
-(function webpackMissingModule() { throw new Error("Cannot find module \"/Users/Charlie/Code/reddit-oop-modular/src/app.scss\""); }());
+__webpack_require__(1);
+module.exports = __webpack_require__(3);
 
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__js_user_adminFunctions_js__ = __webpack_require__(2);
+// import 'js/user_prototype.js';
+
+//console.log('something');
+Object(__WEBPACK_IMPORTED_MODULE_0__js_user_adminFunctions_js__["a" /* default */])();
+//import * as thing from '/js/user_adminFunctions';
+// import 'js/dom_objects.js';
+// import 'js/dom_functions.js';
+// import 'js/_global.js';
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = test;
+// admin functions
+
+var Admin = {
+    createUser: function createUser(username, password) {
+        users.push(new User(username, password));
+    },
+
+    findUser: function findUser(username) {
+        console.log(users.filter(function (user) {
+            return user.username == username;
+        }));
+    },
+
+    getAllPosts: function getAllPosts() {
+        var allPosts = [];
+        users.forEach(function (user) {
+            user.posts.forEach(function (post) {
+                allPosts.push(post);
+            });
+        });
+        return allPosts;
+    },
+
+    getThisPost: function getThisPost(post_id) {
+        return getAllPosts().find(function (post) {
+            return post.post_id == post_id;
+        });
+    },
+
+    getUserFromPost: function getUserFromPost(post_id) {
+        //debugger;
+
+        var thisPost = getAllPosts().find(function (post) {
+            return post.post_id == post_id;
+        });
+        console.log('Posted by', thisPost.user);
+        return thisPost.user;
+    },
+
+    sortByVotes: function sortByVotes(type, id) {}
+
+    // sample users
+    //createUser("charlie","pass");
+    //createUser("coolguy1","hunter2");
+
+
+};function test() {
+    console.log('anything');
+}
+
+// export {Admin};
+//export var k = 1;
+//export default "TEST";
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
